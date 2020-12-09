7551f232103d5d54b06e44ee2f2717f2a7bf5126
PeterColumbus <71803490+PeterColumbus@users.noreply.github.com>
2020-12-10 03:28:07 +0700

    search fine

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ProvinsiService} from '../provinsi.service';
+import {map} from 'rxjs/operators';
+import {getStringInitializerFromProperty} from 'codelyzer/util/astQuery';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.page.scss'],
 })
 export class SearchPage implements OnInit {
+  datas: any[];
+  datas2: any[];
+  constructor(
+      private provinsiSrv: ProvinsiService
+  ) {}
 
-  constructor() { }
 
   ngOnInit() {
+    this.provinsiSrv.getAll().snapshotChanges().pipe(
+        map(changes =>
+          changes.map(c => ({key: c.payload.key, ...c.payload.val()}))
+        )
+    ).subscribe(data => {
+      this.datas2 = data;
+      this.datas = data;
+    });
   }
+  filterList(evt){
+    this.datas = this.datas2;
+    const searchTerm = evt.srcElement.value;
 
+    if (!searchTerm) {
+      return;
+    }
+
+    this.datas = this.datas.filter(currentData => {
+      if (searchTerm) {
+        return (currentData.nama.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1
+            || currentData.makanan.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1
+            || currentData.lagu.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1);
+      }
+    });
+  }
 }
